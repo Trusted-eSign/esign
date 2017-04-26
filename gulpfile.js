79b3e3a690bbd95e69fc311975f9e10dce35cc61
Aleksandr Gavrilov <alg@digt.ru>
2017-04-26 16:01:54 +0300

    add support es2015 uglify. fix #1
    use new module: uglify-js-harmony

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@
 var gulp = require("gulp");
 var typescript = require("typescript");
 var ts = require("gulp-typescript");
+var uglifyjs = require("uglify-js-harmony");
+var minifier = require("gulp-uglify/minifier");
 var uglify = require("gulp-uglify");
 var source = require("vinyl-source-stream");
 var del = require("del");
@@ -24,65 +26,72 @@ var electronPlatform;
 
 var project = ts.createProject("tsconfig.json", { typescript: typescript });
 
-gulp.task("clean", function () {
+gulp.task("clean", function() {
   return del(["build", "tmp", "tmp_app_build"]);
 });
 
-gulp.task("compile", ["clean"], function () {
+gulp.task("compile", ["clean"], function() {
   return gulp.src(["src/" + TARGET + "/**/*.tsx", "src/" + TARGET + "/**/*.ts", "typings/" + TARGET + "/**/*.d.ts"])
     .pipe(project())
     .pipe(gulp.dest("tmp"));
 });
 
-gulp.task("bundle", ["compile"], function () {
+gulp.task("bundle", ["compile"], function() {
   return browserify("tmp/main.js")
     .bundle()
     .pipe(source("bundle_" + TARGET + ".js"))
     .pipe(gulp.dest("tmp"));
 });
 
-gulp.task("minimize", ["bundle"], function () {
+gulp.task("minimize", ["bundle"], function() {
+  var options = {
+    preserveComments: "license"
+  };
+
   return gulp.src(["tmp/bundle_" + TARGET + ".js"])
-    .pipe(uglify())
+    .pipe(minifier(options, uglifyjs))
+    .on("error", function(err) {
+      gutil.log(gutil.colors.red("[Error]"), err.toString());
+    })
     .pipe(gulp.dest("resources/" + TARGET + ""));
 });
 
-gulp.task("no-minimize", ["bundle"], function () {
+gulp.task("no-minimize", ["bundle"], function() {
   return gulp.src(["tmp/bundle_" + TARGET + ".js"])
     .pipe(gulp.dest("resources/" + TARGET + ""));
 });
 
-gulp.task("CryptoARM (win64)", ["no-minimize"], function () {
+gulp.task("CryptoARM (win64)", ["no-minimize"], function() {
   return gulp.src(["resources/" + TARGET + "/**/**", "node_modules_by_system/win64/**/**"])
     .pipe(gulp.dest("tmp_app_build/win64"));
 });
 
-gulp.task("trusted-crypto (win64) copy", ["CryptoARM (win64)"], function () {
+gulp.task("trusted-crypto (win64) copy", ["CryptoARM (win64)"], function() {
   return gulp.src(["../trusted-crypto/win64/**/**"])
     .pipe(gulp.dest("tmp_app_build/win64/node_modules"));
 });
 
-gulp.task("CryptoARM (linux32)", ["minimize"], function () {
+gulp.task("CryptoARM (linux32)", ["minimize"], function() {
   return gulp.src(["resources/" + TARGET + "/**/**", "node_modules_by_system/linux32/**/**"])
     .pipe(gulp.dest("tmp_app_build/linux32"));
 });
 
-gulp.task("trusted-crypto (linux32) copy", ["CryptoARM (linux32)"], function () {
+gulp.task("trusted-crypto (linux32) copy", ["CryptoARM (linux32)"], function() {
   return gulp.src(["../trusted-crypto/linux32/**/**"])
     .pipe(gulp.dest("tmp_app_build/linux32/node_modules"));
 });
 
-gulp.task("CryptoARM (linux64)", ["minimize"], function () {
+gulp.task("CryptoARM (linux64)", ["minimize"], function() {
   return gulp.src(["resources/" + TARGET + "/**/**", "node_modules_by_system/linux64/**/**"])
     .pipe(gulp.dest("tmp_app_build/linux64"));
 });
 
-gulp.task("trusted-crypto (linux64) copy", ["CryptoARM (linux64)"], function () {
+gulp.task("trusted-crypto (linux64) copy", ["CryptoARM (linux64)"], function() {
   return gulp.src(["../trusted-crypto/linux64/**/**"])
     .pipe(gulp.dest("tmp_app_build/linux64/node_modules"));
 });
 
-gulp.task("nw linux64", ["CryptoARM (linux64)", "trusted-crypto (linux64) copy"], function () {
+gulp.task("nw linux64", ["CryptoARM (linux64)", "trusted-crypto (linux64) copy"], function() {
   var nw = new NwBuilder({
     version: NWJS_VERSION,
     files: "./tmp_app_build/linux64/**/**",
@@ -90,16 +99,16 @@ gulp.task("nw linux64", ["CryptoARM (linux64)", "trusted-crypto (linux64) copy"]
     platforms: ["linux64"]
   });
 
-  nw.on("log", function (msg) {
+  nw.on("log", function(msg) {
     gutil.log("nw-builder", msg);
   });
 
-  return nw.build().catch(function (err) {
+  return nw.build().catch(function(err) {
     gutil.log("nw-builder", err);
   });
 });
 
-gulp.task("nw linux32", ["CryptoARM (linux32)", "trusted-crypto (linux32) copy"], function () {
+gulp.task("nw linux32", ["CryptoARM (linux32)", "trusted-crypto (linux32) copy"], function() {
   var nw = new NwBuilder({
     version: NWJS_VERSION,
     files: "./tmp_app_build/linux32/**/**",
@@ -107,16 +116,16 @@ gulp.task("nw linux32", ["CryptoARM (linux32)", "trusted-crypto (linux32) copy"]
     platforms: ["linux32"]
   });
 
-  nw.on("log", function (msg) {
+  nw.on("log", function(msg) {
     gutil.log("nw-builder", msg);
   });
 
-  return nw.build().catch(function (err) {
+  return nw.build().catch(function(err) {
     gutil.log("nw-builder", err);
   });
 });
 
-gulp.task("nw win64 SDK", ["CryptoARM (win64)", "trusted-crypto (win64) copy"], function () {
+gulp.task("nw win64 SDK", ["CryptoARM (win64)", "trusted-crypto (win64) copy"], function() {
   var nw = new NwBuilder({
     version: NWJS_VERSION,
     flavor: "sdk",
@@ -125,11 +134,11 @@ gulp.task("nw win64 SDK", ["CryptoARM (win64)", "trusted-crypto (win64) copy"], 
     platforms: ["win64"]
   });
 
-  nw.on("log", function (msg) {
+  nw.on("log", function(msg) {
     gutil.log("nw-builder", msg);
   });
 
-  return nw.build().catch(function (err) {
+  return nw.build().catch(function(err) {
     gutil.log("nw-builder", err);
   });
 });
@@ -145,37 +154,37 @@ gulp.task("nwdebug", [
 /*
 ---------------electron build app------------------
 */
-gulp.task("electron set platform win32-x64", function () {
+gulp.task("electron set platform win32-x64", function() {
   electronPlatform = "win32-x64";
 });
-gulp.task("electron set platform win32-ia32", function () {
+gulp.task("electron set platform win32-ia32", function() {
   electronPlatform = "win32-ia32";
 });
-gulp.task("electron set platform linux-x64", function () {
+gulp.task("electron set platform linux-x64", function() {
   electronPlatform = "linux-x64";
 });
-gulp.task("electron set platform linux-ia32", function () {
+gulp.task("electron set platform linux-ia32", function() {
   electronPlatform = "linux-ia32";
 });
-gulp.task("electron set platform darwin-x64", function () {
+gulp.task("electron set platform darwin-x64", function() {
   electronPlatform = "darwin-x64";
 });
 
-gulp.task("Electron resources copy", ["minimize"], function () {
+gulp.task("Electron resources copy", ["minimize"], function() {
   return gulp.src(["resources/**/**", "node_modules_by_system/win64/**/**"])
     .pipe(gulp.dest("./tmp_app_build/electron-" + electronPlatform));
 });
 
-gulp.task("trusted-crypto electron copy", ["Electron resources copy"], function () {
+gulp.task("trusted-crypto electron copy", ["Electron resources copy"], function() {
   return gulp.src(["./cache/trusted-crypto/" + electronPlatform + "/**/**"])
     .pipe(gulp.dest("./tmp_app_build/electron-" + electronPlatform + "/node_modules"));
 });
 
-gulp.task("build electron", ["trusted-crypto electron copy"], function () {
+gulp.task("build electron", ["trusted-crypto electron copy"], function() {
   return gulp.src("")
     .pipe(electron({
       src: "./tmp_app_build/electron-" + electronPlatform,
-      packageJson: packageJson, browserify,
+      packageJson: packageJson,
       release: "./build/Electron",
       cache: "./cache/electron",
       version: ELECTRON_VERSION,
@@ -194,12 +203,12 @@ gulp.task("build electron", ["trusted-crypto electron copy"], function () {
     .pipe(gulp.dest(""));
 });
 
-gulp.task("cert copy", ["build electron"], function () {
+gulp.task("cert copy", ["build electron"], function() {
   return gulp.src(["./resources/Trusted_eSign/cert1.crt", "./resources/Trusted_eSign/cert1.key"])
     .pipe(gulp.dest("./build/Electron/" + ELECTRON_VERSION + "/" + electronPlatform));
 });
 
-gulp.task("remove app folder", ["build electron"], function () {
+gulp.task("remove app folder", ["build electron"], function() {
   return del(["./build/Electron/" + ELECTRON_VERSION + "/" + electronPlatform + "/resources/app"]);
 });
 
