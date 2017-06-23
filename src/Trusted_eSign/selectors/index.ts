58f60e1f55057326d219db57b93946df4f9be6f7
Aleksandr Gavrilov <alg@digt.ru>
2017-06-23 17:19:17 +0300

    fix certificate filter
    add lowerCase

--- src/Trusted_eSign/selectors/index.ts
+++ src/Trusted_eSign/selectors/index.ts
@@ -7,6 +7,18 @@ export const idGetter = (state, props) => props.id;
 
 export const filteredCertificatesSelector = createSelector(certificatesGetter, filtersGetter, (certificates, filters) => {
   const { searchValue } = filters;
+  const search = searchValue.toLowerCase();
 
-  return mapToArr(certificates).filter((certificate) => certificate.subjectFriendlyName.toLowerCase().match(searchValue));
+  return mapToArr(certificates).filter((certificate) => {
+    return (
+      certificate.hash.toLowerCase().match(search) ||
+      certificate.issuerFriendlyName.toLowerCase().match(search) ||
+      certificate.subjectFriendlyName.toLowerCase().match(search) ||
+      certificate.serial.toLowerCase().match(search) ||
+      certificate.notAfter.toLowerCase().match(search) ||
+      certificate.notBefore.toLowerCase().match(search) ||
+      certificate.organizationName.toLowerCase().match(search) ||
+      certificate.signatureAlgorithm.toLowerCase().match(search)
+    );
+  });
 });
