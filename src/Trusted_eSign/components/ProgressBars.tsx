87bedb5276e2903b209c371a8b926edd9ec9bd8e
Aleksandr Gavrilov <alg@digt.ru>
2017-06-23 17:17:50 +0300

    move Preloader to ProgressBars

--- src/Trusted_eSign/components/ProgressBars.tsx
+++ src/Trusted_eSign/components/ProgressBars.tsx
@@ -1,7 +1,23 @@
 import * as React from "react";
+import { lang } from "../module/global_app";
 
 export default function ProgressBars() {
   return (
-    <h1>LOAD</h1>
+    <div className="preloader-content">
+      <div className="preloader-wrapper big active">
+        <div className="spinner-layer spinner-blue-only">
+          <div className="circle-clipper left">
+            <div className="circle"></div>
+          </div>
+          <div className="gap-patch">
+            <div className="circle" />
+          </div>
+          <div className="circle-clipper right">
+            <div className="circle" />
+          </div>
+        </div>
+      </div>
+      <div className="preloader-text">{lang.get_resource.Settings.wait}</div>
+    </div>
   );
 }
--- src/Trusted_eSign/components/components.tsx
+++ src/Trusted_eSign/components/components.tsx
@@ -914,26 +914,7 @@ export class Password extends React.Component<any, any> {
         </div>;
     }
 }
-export class Preloader extends React.Component<any, any> {
-    render() {
-        return <div className="preloader-content">
-            <div className="preloader-wrapper big active">
-                <div className="spinner-layer spinner-blue-only">
-                    <div className="circle-clipper left">
-                        <div className="circle"></div>
-                    </div>
-                    <div className="gap-patch">
-                        <div className="circle" />
-                    </div>
-                    <div className="circle-clipper right">
-                        <div className="circle" />
-                    </div>
-                </div>
-            </div>
-            <div className="preloader-text">{lang.get_resource.Settings.wait}</div>
-        </div>;
-    }
-};
+
 export class Dialog extends React.Component<any, any> {
     constructor(props: any) {
         super(props);
