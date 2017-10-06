cfa5187b07c6fba4f76b0582bbbf238e08cd1ad2
Aleksandr Gavrilov <alg@digt.ru>
2017-10-06 16:07:20 +0300

    format code

--- app/components/License/LicenseSetupModal.tsx
+++ app/components/License/LicenseSetupModal.tsx
@@ -167,43 +167,43 @@ class LicenseSetupModal extends React.Component<ILicenseSetupModalProps, ILicens
     const disable = this.state.license_file || this.state.license_key ? "" : "disabled";
 
     return (
-    <div id="add-licence-key" className="modal licence-modal">
-      <div className="licence-modal-main">
-        <HeaderWorkspaceBlock text={localize("License.enter_key", locale)} new_class="modal-bar" icon="close" onСlickBtn={this.props.closeWindow.bind(this)} />
-        <div className="licence-modal-content">
-          <div className="license-key">
-            <div className="input-field col s6 input-field-licence">
-              <i className="material-icons prefix key-prefix">vpn_key</i>
-              <textarea id="input_key" className="materialize-textarea" value={this.state.license_key} onChange={function(e: any) {
-                self.setState({ license_file: self.state.license_file, license_key: e.target.value });
-              }} />
-              <label htmlFor="input_key">{localize("License.entered_the_key", locale)}</label>
-              <a className="nav-small-btn waves-effect" onClick={this.paste.bind(this)}>
-                <i className="nav-small-icon material-icons">content_copy</i>
-              </a>
+      <div id="add-licence-key" className="modal licence-modal">
+        <div className="licence-modal-main">
+          <HeaderWorkspaceBlock text={localize("License.enter_key", locale)} new_class="modal-bar" icon="close" onСlickBtn={this.props.closeWindow.bind(this)} />
+          <div className="licence-modal-content">
+            <div className="license-key">
+              <div className="input-field col s6 input-field-licence">
+                <i className="material-icons prefix key-prefix">vpn_key</i>
+                <textarea id="input_key" className="materialize-textarea" value={this.state.license_key} onChange={function(e: any) {
+                  self.setState({ license_file: self.state.license_file, license_key: e.target.value });
+                }} />
+                <label htmlFor="input_key">{localize("License.entered_the_key", locale)}</label>
+                <a className="nav-small-btn waves-effect" onClick={this.paste.bind(this)}>
+                  <i className="nav-small-icon material-icons">content_copy</i>
+                </a>
+              </div>
             </div>
-          </div>
-          <div className="or">
-            {localize("Common.or", locale)}
-          </div>
-          <div className="license-file">
-            <div className="input-field col s6 input-field-licence">
-              <i className="material-icons prefix key-prefix">vpn_key</i>
-              <input id="input_file" type="text" value={this.state.license_file} onChange={function(e: any) {
-                self.setState({ license_file: e.target.value, license_key: self.state.license_key });
-              }} />
-              <label htmlFor="input_file">{localize("License.lic_file_choose", locale)}</label>
-              <a className="nav-small-btn waves-effect" onClick={this.openLicenseFile.bind(this)}>
-                <i className="nav-small-icon material-icons">insert_drive_file</i>
-              </a>
+            <div className="or">
+              {localize("Common.or", locale)}
             </div>
-          </div>
-          <div className="license-btn">
-            <a className={"waves-effect waves-light btn " + disable} onClick={this.setupLicense.bind(this)}>{localize("License.Entered", locale)}</a>
+            <div className="license-file">
+              <div className="input-field col s6 input-field-licence">
+                <i className="material-icons prefix key-prefix">vpn_key</i>
+                <input id="input_file" type="text" value={this.state.license_file} onChange={function(e: any) {
+                  self.setState({ license_file: e.target.value, license_key: self.state.license_key });
+                }} />
+                <label htmlFor="input_file">{localize("License.lic_file_choose", locale)}</label>
+                <a className="nav-small-btn waves-effect" onClick={this.openLicenseFile.bind(this)}>
+                  <i className="nav-small-icon material-icons">insert_drive_file</i>
+                </a>
+              </div>
+            </div>
+            <div className="license-btn">
+              <a className={"waves-effect waves-light btn " + disable} onClick={this.setupLicense.bind(this)}>{localize("License.Entered", locale)}</a>
+            </div>
           </div>
         </div>
       </div>
-    </div>
     );
   }
 }
