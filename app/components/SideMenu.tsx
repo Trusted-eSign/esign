45a8e51f979e4c9604e667917a4bfd7101c1463f
Aleksandr Gavrilov <alg@digt.ru>
2017-11-16 11:53:43 +0300

    code styling for Side Menu
    goto PureComponent and del inline stiles

--- app/components/SideMenu.tsx
+++ app/components/SideMenu.tsx
@@ -2,51 +2,63 @@ import PropTypes from "prop-types";
 import * as React from "react";
 import { Link } from "react-router";
 
-const MenuStyle = {
-  textDecoration: "none",
-};
-
-class SideMenu extends React.Component<any, any> {
+class SideMenu extends React.PureComponent {
   static contextTypes = {
     locale: PropTypes.string,
     localize: PropTypes.func,
   };
 
-  constructor(props: any) {
-    super(props);
-    this.state = { open: false };
-  }
-
-  handleToggle() {
-    this.setState({ open: !this.state.open });
-  }
-
-  handleClose() {
-    this.setState({ open: false });
+  shouldComponentUpdate(nextProps: {}, nextState: {}, nextContext: { locale: string }) {
+    return (this.context.locale !== nextContext.locale) ? true : false;
   }
 
   render() {
     const { localize, locale } = this.context;
-    const settings = {
-      draggable: false,
-    };
 
     return (
       <div>
         <nav className="menu-logo">
           <div className="center">
-            <Link to="/" className="menuheaderlink" href="/" {...settings}>
-              <i className="logo-trusted"></i><div className="logo-text">{localize("About.product_NAME", locale)}</div>
+            <Link to="/" className="menuheaderlink" href="/">
+              <i className="logo-trusted" />
+              <div className="logo-text">
+                {localize("About.product_NAME", locale)}
+              </div>
             </Link>
           </div>
         </nav>
-        <Link to="/sign" {...settings} style={MenuStyle}>{localize("Sign.Sign", locale)}<i className="material-icons left sign">mode_edit</i></Link>
-        <Link to="/encrypt" {...settings} style={MenuStyle}>{localize("Encrypt.Encrypt", locale)}<i className="material-icons left encrypt">enhanced_encryption</i></Link>
-        <Link to="/certificate" {...settings} style={MenuStyle}>{localize("Certificate.Certificate", locale)}<i className="material-icons left cert">library_books</i></Link>
-        <div className="menu-elements">
-          <Link className={"bordered--top"} {...settings} to="/about" style={MenuStyle}>{localize("About.About", locale)}<i className="material-icons left about">about</i></Link>
-          <Link to="/license" {...settings} style={MenuStyle}>{localize("License.License", locale)}<i className="material-icons left license">license</i></Link>
-          <Link to="/help" {...settings} style={MenuStyle}>{localize("Help.Help", locale)}<i className="material-icons left help">help</i></Link>
+        <div className="row">
+          <Link to="/sign">
+            {localize("Sign.Sign", locale)}
+            <i className="material-icons left sign">mode_edit</i>
+          </Link>
+          <Link to="/encrypt">
+            {localize("Encrypt.Encrypt", locale)}
+            <i className="material-icons left encrypt">enhanced_encryption</i>
+          </Link>
+
+          <Link to="/certificate">
+            {localize("Certificate.Certificate", locale)}
+            <i className="material-icons left cert">library_books</i>
+          </Link>
+        </div>
+        <div className="row">
+          <div className="menu-elements">
+            <div className="row">
+              <Link className={"bordered--top"} to="/about">
+                {localize("About.About", locale)}
+                <i className="material-icons left about">about</i>
+              </Link>
+              <Link to="/license">
+                {localize("License.License", locale)}
+                <i className="material-icons left license">license</i>
+              </Link>
+              <Link to="/help">
+                {localize("Help.Help", locale)}
+                <i className="material-icons left help">help</i>
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     );
