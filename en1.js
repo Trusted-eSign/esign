c746b2fcb5dba1c253e387616a4f61ad7a0c8a7c
Sergey Tikhomirov <32257840+TikhomirovSergei@users.noreply.github.com>
2017-12-05 16:30:15 +0300

    Add files via upload
    Меню урезано. Ссылки в подпункте "Help" ведут на "https://github.com/TrustedPlus/esign". Добавлена локализация для меню. При нажатии на крестик приложение нормально заканчивает работу.

---
+++ en1.js
@@ -0,0 +1,17 @@
+{
+  "Inspect element": "Inspect element",
+  "Electron": "CryptoARM GOST",
+  "Quit": "Quit",
+  "Window": "Window",
+  "Minimize": "Minimize",
+  "Close": "Close",
+  "Bring All to Front": "Bring All to Front",
+  "Help": "Help",
+  "Learn More": "Learn More",
+  "Documentation": "Documentation",
+  "Community Discussions": "Community Discussions",
+  "Search Issues": "Search Issues",
+  "&File": "File",
+  "&Open": "Open",
+  "&Close": "Close"
+}
