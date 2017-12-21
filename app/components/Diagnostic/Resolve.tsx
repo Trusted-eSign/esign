57f836eefa7a56b200e2750a9b1b733ad9c90c45
shesnokov <shesnokov@gmail.com>
2017-12-21 11:40:10 +0300

    add template resolve

--- app/components/Diagnostic/Resolve.tsx
+++ app/components/Diagnostic/Resolve.tsx
@@ -19,6 +19,49 @@ class Resolve extends React.Component<any, any> {
     this.setState({ certForInfo: certificate });
   }
 
+ 
+  Resolvers = {
+    problem_1: function() {
+      //Решение проблемы отсутствия КриптоПро CSP
+      const { localize, locale } = this.context;
+      return (
+        <div>
+         
+        </div>
+      );
+    },
+  
+    problem_2: function() {
+      //Решение проблемы отсутствия лицензии на КриптоПро CSP
+      const { localize, locale } = this.context;
+      return (
+        <div>
+         
+        </div>
+      );
+    },
+  
+    problem_3: function() {
+      //Решение проблемы отсутствия лицензии на приложение
+      const { localize, locale } = this.context;
+      return (
+        <div>
+         
+        </div>
+      );
+    },
+
+    problem_4: function() {
+      //Решение проблемы отсутствия сертификатов
+      const { localize, locale } = this.context;
+      return (
+        <div>
+         
+        </div>
+      );
+    }
+  }
+  
   getResolve() {
     const { certForInfo } = this.state;
     const { localize, locale } = this.context;
@@ -28,7 +71,7 @@ class Resolve extends React.Component<any, any> {
         <HeaderWorkspaceBlock text={localize("Diagnostic.resolve_header", locale)} />
         <div className="add-problems">
           <div className="add-problems-item">
-            {/* <CertificateInfo certificate={certForInfo} /> */}
+            {this.Resolvers[0]}
           </div>
         </div>;
       </div>
