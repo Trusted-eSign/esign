fe538892e568a05ce6aa486e6d79884b2439c725
Aleksandr Gavrilov <alg@digt.ru>
2017-12-27 13:48:36 +0300

    add check in preloaded state

--- app/store/preloadedState.ts
+++ app/store/preloadedState.ts
@@ -3,11 +3,18 @@ import * as path from "path";
 import { SETTINGS_JSON } from "../constants";
 import { fileExists } from "../utils";
 
-let odata;
+let odata = {};
 
 if (fileExists(SETTINGS_JSON)) {
   const data = fs.readFileSync(SETTINGS_JSON, "utf8");
-  odata = JSON.parse(data);
+
+  if (data) {
+    try {
+      odata = JSON.parse(data);
+    } catch (e) {
+      odata = {};
+    }
+  }
 }
 
 export default odata;
