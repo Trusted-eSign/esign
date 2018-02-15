8edd04faa9a3df3d629a925ee1d094c4e8ef6200
Aleksandr Gavrilov <alg@digt.ru>
2018-02-15 13:02:28 +0300

    fix file list style

--- app/components/FileListItem.tsx
+++ app/components/FileListItem.tsx
@@ -80,29 +80,27 @@ class FileListItem extends React.Component<IFilelistItemProps, {}> {
     }
 
     return (
-      <div style={style}>
-        <div className={"collection-item avatar files-collection " + active} id={"file-" + this.props.index} onClick={this.props.onClickBtn}>
-          <div className="r-iconbox-link">
-            <div className="r-iconbox-icon"><i className={file.extension} id="file-avatar"></i></div>
-            <p className="collection-title">{file.filename}</p>
-            <p className="collection-info">{file.lastModifiedDate.toLocaleDateString(locale, {
-              year: "numeric",
-              month: "long",
-              day: "numeric",
-              hour: "numeric",
-              minute: "numeric",
-              second: "numeric",
-            })}</p>
-          </div>
-          {this.getSignatureStatusCicrcle()}
-          <i className="file-setting-item waves-effect material-icons secondary-content"
-            data-activates={"dropdown-btn-set-file-" + this.props.index} onClick={self.stopEvent}>more_vert</i>
-          <ul id={"dropdown-btn-set-file-" + this.props.index} className="dropdown-content">
-            <li><a onClick={function (event: any) { self.openFile(event, file.fullpath); }}>{localize("Settings.open_file", locale)}</a></li>
-            <li><a onClick={function (event: any) { self.openFileFolder(event, file.fullpath); }}>{localize("Settings.go_to_file", locale)}</a></li>
-            <li><a onClick={this.props.removeFiles}>{localize("Settings.delete_file", locale)}</a></li>
-          </ul>
+      <div className={"collection-item avatar files-collection " + active} id={"file-" + this.props.index} onClick={this.props.onClickBtn}>
+        <div className="r-iconbox-link">
+          <div className="r-iconbox-icon"><i className={file.extension} id="file-avatar"></i></div>
+          <p className="collection-title">{file.filename}</p>
+          <p className="collection-info">{file.lastModifiedDate.toLocaleDateString(locale, {
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+            hour: "numeric",
+            minute: "numeric",
+            second: "numeric",
+          })}</p>
         </div>
+        {this.getSignatureStatusCicrcle()}
+        <i className="file-setting-item waves-effect material-icons secondary-content"
+          data-activates={"dropdown-btn-set-file-" + this.props.index} onClick={self.stopEvent}>more_vert</i>
+        <ul id={"dropdown-btn-set-file-" + this.props.index} className="dropdown-content">
+          <li><a onClick={function (event: any) { self.openFile(event, file.fullpath); }}>{localize("Settings.open_file", locale)}</a></li>
+          <li><a onClick={function (event: any) { self.openFileFolder(event, file.fullpath); }}>{localize("Settings.go_to_file", locale)}</a></li>
+          <li><a onClick={this.props.removeFiles}>{localize("Settings.delete_file", locale)}</a></li>
+        </ul>
       </div>
     );
   }
