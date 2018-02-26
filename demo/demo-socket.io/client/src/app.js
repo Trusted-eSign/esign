d8af47d0e79d9cd5787b589bef23a5a7656a821e
Aleksandr Gavrilov <alg@digt.ru>
2018-02-26 17:52:53 +0300

    upp demo socket

--- demo/demo-socket.io/client/src/app.js
+++ demo/demo-socket.io/client/src/app.js
@@ -1,9 +1,36 @@
 import io from 'socket.io-client';
 
 var socket = io.connect('http://localhost:4040');
-var form = document.getElementById('form');
-var formencrypt = document.getElementById('formencrypt');
-var formbitrix = document.getElementById('formbitrix');
+var formsign = document.getElementById('sign');
+
+var file1 = {};
+file1.name = 'filename1.txt';
+file1.sys_name = 'filename1.txt';
+file1.url = 'https://bitrix.tsumo.org/bitrix/components/trustednet/trustednet.docs/ajax.php?command=content&id=1'
+file1.id = 1;
+
+var file2 = {};
+file2.name = 'filename2.txt';
+file2.sys_name = 'filename2.txt.p7s';
+file2.url = 'https://bitrix.tsumo.org/bitrix/components/trustednet/trustednet.docs/ajax.php?command=content&id=2'
+file2.id = 2;
+
+var file3 = {};
+file3.name = 'filename3.txt';
+file3.sys_name = 'filename3.txt';
+file3.url = 'https://bitrix.tsumo.org/bitrix/components/trustednet/trustednet.docs/ajax.php?command=content&id=3'
+file3.id = 3;
+
+var signRequest = {};
+signRequest.jsonrpc = '2.0';
+signRequest.method = 'sign';
+signRequest.params = {};
+signRequest.params.token = '';
+signRequest.params.files = [file1, file2, file3];
+signRequest.params.extra = {};
+signRequest.params.uploader = 'https://bitrix.tsumo.org/bitrix/components/trustednet/trustednet.sign/ajax.php?command=upload';
+signRequest.params.cancel = 'https://bitrix.tsumo.org/bitrix/components/trustednet/trustednet.sign/ajax.php?command=updateStatus&status=2';
+signRequest.params.error = 'https://bitrix.tsumo.org/bitrix/components/trustednet/trustednet.sign/ajax.php?command=updateStatus&status=3';
 
 socket.on('files signed', function (data) {
 	console.log('файл подписан', data);
@@ -17,62 +44,8 @@ socket.on('signature verified', function (data) {
 	console.log('информация о подписи', data);
 });
 
-form.onsubmit = function(ev) {
-	ev.preventDefault();
-	
-	var fileEl = document.getElementById('file');
-	const file = fileEl.files[0];
-	const name = fileEl.files[0].name;
-	socket.emit("sign file", name);
-
-	var stream = ss.createStream();
-
-	// upload a file to the server.
-	ss(socket).emit('file stream', stream, {size: file.size, name: name});
-	var blobStream =  ss.createBlobReadStream(file);
-
-	var size = 0;
-
-	blobStream.on('data', function (chunk) {
-		size += chunk.length;
-		console.log(Math.floor(size / file.size * 100) + '%');
-		// -> e.g. '42%'
-	});
-
-	
-	blobStream.pipe(stream);
-};
-
-formencrypt.onsubmit = function(ev) {
-	ev.preventDefault();
-	
-	var fileEl = document.getElementById('fileencrypt');
-	const file = fileEl.files[0];
-	const name = fileEl.files[0].name;
-
-	socket.emit("encrypt file", name);
-
-	var stream = ss.createStream();
-
-	// upload a file to the server.
-	ss(socket).emit('file stream', stream, {size: file.size, name: name});
-	var blobStream = ss.createBlobReadStream(file);
-
-
-	var size = 0;
-
-	blobStream.on('data', function (chunk) {
-		size += chunk.length;
-		console.log(Math.floor(size / file.size * 100) + '%');
-		// -> e.g. '42%'
-	});
-
-	
-	blobStream.pipe(stream);
-};
-
-formbitrix.onsubmit = function(ev) {
+formsign.onsubmit = function(ev) {
 	ev.preventDefault();
 
-	socket.emit("bitrix", {url: "https://bitrix.tsumo.org/bitrix/components/trustednet/trustednet.docs/ajax.php?command=content&id=1", name: "file1.txt"});
+	socket.emit("sign", signRequest);
 }
