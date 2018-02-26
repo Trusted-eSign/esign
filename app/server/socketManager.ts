d28b324cde26d64b209994e3aa90e2817577ead8
Aleksandr Gavrilov <alg@digt.ru>
2018-02-26 17:53:10 +0300

    upp socket manager

--- app/server/socketManager.ts
+++ app/server/socketManager.ts
@@ -3,82 +3,86 @@ import * as fs from "fs";
 import * as os from "os";
 import * as path from "path";
 import { push } from "react-router-redux";
-import { selectFile } from "../AC";
 import store from "../store/index";
 import { extFile, toBase64 } from "../utils";
 import io from "./socketIO";
 
 // tslint:disable-next-line:no-var-requires
-const ss = require("socket.io-stream");
-// tslint:disable-next-line:no-var-requires
 const request = require("request");
 const TMP_DIR = os.tmpdir();
 
-io.on("connection", function (socket) {
+interface IFileProperty {
+  name: string;
+  sys_name: string;
+  url: string;
+  id: string;
+}
+
+interface ISignRequest {
+  id: string;
+  method: string;
+  params: {
+    token: string;
+    files: IFileProperty[];
+    uploader: string;
+  };
+  controller: string;
+}
+
+io.on("connection", function(socket) {
   // tslint:disable-next-line:no-console
   store.dispatch({ type: "ADD_CONNECTION", payload: {id: socket.id, socket }});
   store.dispatch({ type: "SET_CONNECTED", payload: {id: socket.id }});
 
-  socket.on("sign file", (file) => {
-    store.dispatch(push("/sign"));
-    mainWindow.show();
-
-    $(".toast-signercert_not_found").remove();
-    Materialize.toast("Выбран файл для полдписи - " + file, 2000, "toast-signercert_not_found");
-
-    setTimeout(() => {
-      socket.emit("files signed", file + ".sig");
-    }, 5000);
-
-  });
-
-  socket.on("encrypt file", (file) => {
-    store.dispatch(push("/encrypt"));
-    mainWindow.show();
-
-    $(".toast-signercert_not_found").remove();
-    Materialize.toast("Выбран файл для шифрования - " + file, 2000, "toast-signercert_not_found");
-  });
-
-  socket.on("bitrix", (data) => {
+  socket.on("sign", (data: ISignRequest) => {
     // tslint:disable-next-line:no-console
-    const { name, url } = data;
+    const { params } = data;
+    const { files } = params;
 
     store.dispatch(push("/sign"));
     mainWindow.show();
+    mainWindow.focus();
 
-    const pathForSave = path.join(TMP_DIR, name);
+    files.forEach((file) => {
+      const pathForSave = path.join(TMP_DIR, file.name);
 
-    download(url, pathForSave, (err: Error, ss, goodPath) => {
-      if (err) {
-        console.log("--err", err);
-      } else {
-        const file = getFileProperty(goodPath);
+      download(file.url, pathForSave, (err: Error, ss, goodPath) => {
+        if (err) {
+          console.log("--err", err);
+        } else {
+          const fileProps = getFileProperty(goodPath);
 
-        store.dispatch({ generateId: true, type: "SELECT_FILE", payload: { file: {...file, socket: socket.id } }});
-      }
+          store.dispatch({ generateId: true, type: "SELECT_FILE", payload: { file: { ...fileProps, socket: socket.id } } });
+        }
+      });
     });
   });
 
-  ss(socket).on("file stream", function(stream, data) {
-    const outpath = path.join(TMP_DIR, data.name);
-    stream.pipe(fs.createWriteStream(outpath));
-    stream.on("end", function() {
-      socket.emit("file saved");
+  socket.on("view", (data: ISignRequest) => {
+    // tslint:disable-next-line:no-console
+    const { params } = data;
+    const { files } = params;
 
-      const file = getFileProperty(outpath);
+    store.dispatch(push("/sign"));
+    mainWindow.show();
+    mainWindow.focus();
 
-      store.dispatch({ generateId: true, type: "SELECT_FILE", payload: { file } });
-    });
-    stream.on("error", function(err) {
-      socket.emit("file failed");
+    files.forEach((file) => {
+      const pathForSave = path.join(TMP_DIR, file.name);
+
+      download(file.url, pathForSave, (err: Error, ss, goodPath) => {
+        if (err) {
+          console.log("--err", err);
+        } else {
+          const fileProps = getFileProperty(goodPath);
+
+          store.dispatch({ generateId: true, type: "SELECT_FILE", payload: { file: { ...fileProps, socket: socket.id } } });
+        }
+      });
     });
   });
 
   socket.on("disconnect", function() {
-    // tslint:disable-next-line:no-console
-    console.log("user disconnected");
-
     store.dispatch({ type: "REMOVE_CONNECTION", payload: {id: socket.id, socket }});
   });
 });
