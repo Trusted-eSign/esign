c88afd73bfd7718c86f2cc3244083e288db05ec2
Aleksandr Gavrilov <alg@digt.ru>
2018-03-06 16:46:17 +0300

    add encrypt and decrypt operations in socket manager

---
+++ app/server/constants.ts
@@ -0,0 +1,13 @@
+export const CONNECTION = "connection";
+export const CONNECT = "connect";
+export const DISCONNECT = "disconnect";
+
+export const SIGN = "sign";
+export const VERIFY = "verify";
+export const ENCRYPT = "encrypt";
+export const DECRYPT = "decrypt";
+
+export const SIGNED = "signed";
+export const VERIFYED = "verifyed";
+export const ENCRYPTED = "encrypted";
+export const DECRYPTED = "decrypted";
--- app/server/socketManager.ts
+++ app/server/socketManager.ts
@@ -3,9 +3,10 @@ import * as fs from "fs";
 import * as os from "os";
 import * as path from "path";
 import { push } from "react-router-redux";
-import { ADD_CONNECTION, REMOVE_CONNECTION, SELECT_FILE, SET_CONNECTED} from "../constants";
+import { ADD_CONNECTION, REMOVE_ALL_FILES, REMOVE_CONNECTION, SELECT_FILE, SET_CONNECTED } from "../constants";
 import store from "../store/index";
 import { extFile, toBase64 } from "../utils";
+import { CONNECTION, DECRYPT, DISCONNECT, ENCRYPT, SIGN, VERIFY } from "./constants";
 import io from "./socketIO";
 
 // tslint:disable-next-line:no-var-requires
@@ -30,63 +31,84 @@ interface ISignRequest {
   controller: string;
 }
 
-io.on("connection", function(socket) {
-  // tslint:disable-next-line:no-console
-  store.dispatch({ type: ADD_CONNECTION, payload: {id: socket.id, socket }});
-  store.dispatch({ type: SET_CONNECTED, payload: {id: socket.id }});
+interface IEncryptRequest {
+  id: string;
+  method: string;
+  params: {
+    token: string;
+    files: IFileProperty[];
+    uploader: string;
+    extra: any;
+  };
+  controller: string;
+}
 
-  socket.on("sign", (data: ISignRequest) => {
-    // tslint:disable-next-line:no-console
-    const { params } = data;
-    const { extra, files } = params;
+io.on(CONNECTION, (socket) => {
+  store.dispatch({ type: ADD_CONNECTION, payload: { id: socket.id, socket } });
+  store.dispatch({ type: SET_CONNECTED, payload: { id: socket.id } });
 
-    store.dispatch(push("/sign"));
-    mainWindow.show();
-    mainWindow.focus();
+  socket.on(SIGN, (data: ISignRequest) => {
+    store.dispatch({ type: REMOVE_ALL_FILES });
 
-    files.forEach((file) => {
-      const pathForSave = path.join(TMP_DIR, file.name);
-
-      download(file.url, pathForSave, (err: Error, ss, goodPath) => {
-        if (err) {
-          console.log("--err", err);
-        } else {
-          const fileProps = getFileProperty(goodPath);
-
-          store.dispatch({ generateId: true, type: SELECT_FILE, payload: { file: { ...fileProps, extra, remoteId: file.id, socket: socket.id } } });
-        }
-      });
-    });
+    openWindow(SIGN);
+    downloadFiles(data, socket.id);
   });
 
-  socket.on("view", (data: ISignRequest) => {
-    // tslint:disable-next-line:no-console
-    const { params } = data;
-    const { files } = params;
+  socket.on(VERIFY, (data: ISignRequest) => {
+    store.dispatch({ type: REMOVE_ALL_FILES });
 
-    store.dispatch(push("/sign"));
-    mainWindow.show();
-    mainWindow.focus();
-
-    files.forEach((file) => {
-      const pathForSave = path.join(TMP_DIR, file.name);
-
-      download(file.url, pathForSave, (err: Error, ss, goodPath) => {
-        if (err) {
-          console.log("--err", err);
-        } else {
-          const fileProps = getFileProperty(goodPath);
-
-          store.dispatch({ generateId: true, type: SELECT_FILE, payload: { file: { ...fileProps, extra: file.extra, socket: socket.id } } });
-        }
-      });
-    });
+    openWindow(VERIFY);
+    downloadFiles(data, socket.id);
   });
 
-  socket.on("disconnect", function() {
-    store.dispatch({ type: REMOVE_CONNECTION, payload: {id: socket.id, socket }});
+  socket.on(ENCRYPT, (data: IEncryptRequest) => {
+    store.dispatch({ type: REMOVE_ALL_FILES });
+
+    openWindow(ENCRYPT);
+    downloadFiles(data, socket.id);
+  });
+
+  socket.on(DECRYPT, (data: IEncryptRequest) => {
+    store.dispatch({ type: REMOVE_ALL_FILES });
+
+    openWindow(DECRYPT);
+    downloadFiles(data, socket.id);
+  });
+
+  socket.on(DISCONNECT, () => {
+    store.dispatch({ type: REMOVE_CONNECTION, payload: { id: socket.id, socket } });
   });
 });
+
+const downloadFiles = (data: ISignRequest | IEncryptRequest, socketId: string) => {
+  const { params } = data;
+
+  if (!params) {
+    return;
+  }
+
+  const { extra, files } = params;
+
+  if (!files || !files.length) {
+    return;
+  }
+
+  files.forEach((file) => {
+    const pathForSave = path.join(TMP_DIR, file.name);
+
+    download(file.url, pathForSave, (err: Error, ss, goodPath) => {
+      if (err) {
+        // tslint:disable-next-line:no-console
+        console.log("--err", err);
+      } else {
+        if (goodPath) {
+          const fileProps = getFileProperty(goodPath);
+          store.dispatch({ generateId: true, type: SELECT_FILE, payload: { file: { ...fileProps, extra, remoteId: file.id, socket: socketId } } });
+        }
+      }
+    });
+  });
+};
 
 function download(url: string, pathname: string, done: (err: Error, url?: string, path?: string) => void): void {
   const sendReq: any = request.get(url);
@@ -94,9 +116,15 @@ function download(url: string, pathname: string, done: (err: Error, url?: string
   sendReq.on("response", (response) => {
     switch (response.statusCode) {
       case 200:
+        const len = parseInt(response.headers["content-length"], 10);
+        let cur = 0;
+        const total = len / 1048576;
+
         const stream = fs.createWriteStream(pathname);
 
         response.on("data", (chunk) => {
+          cur += chunk.length;
+          console.log("Downloading " + (100.0 * cur / len).toFixed(2) + "% " + (cur / 1048576).toFixed(2) + " mb\r" + ".<br/> Total size: " + total.toFixed(2) + " mb");
           stream.write(chunk);
         }).on("end", () => {
           stream.on("close", () => {
@@ -117,7 +145,7 @@ function download(url: string, pathname: string, done: (err: Error, url?: string
   });
 }
 
-function getFileProperty(filepath: string) {
+const getFileProperty = (filepath: string) => {
   const stat = fs.statSync(filepath);
 
   const extension = extFile(filepath);
@@ -129,4 +157,24 @@ function getFileProperty(filepath: string) {
     lastModifiedDate: stat.birthtime,
     size: stat.size,
   };
-}
+};
+
+const openWindow = (operation: string) => {
+  switch (operation) {
+    case SIGN:
+    case VERIFY:
+      store.dispatch(push("/sign"));
+      return;
+
+    case ENCRYPT:
+    case DECRYPT:
+      store.dispatch(push("/encrypt"));
+      return;
+
+    default:
+      return;
+  }
+
+  mainWindow.show();
+  mainWindow.focus();
+};
