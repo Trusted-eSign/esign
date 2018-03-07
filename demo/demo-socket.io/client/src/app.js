156733173642e6ae7d78f6dd741e3d36cac71e4a
Aleksandr Gavrilov <alg@digt.ru>
2018-03-07 10:31:44 +0300

    update demo socket
    use local files for tests

--- demo/demo-socket.io/client/src/app.js
+++ demo/demo-socket.io/client/src/app.js
@@ -5,20 +5,17 @@ var formsign = document.getElementById('sign');
 
 var file1 = {};
 file1.name = 'filename1.txt';
-file1.sys_name = 'filename1.txt';
-file1.url = 'https://bitrix.tsumo.org/bitrix/components/trustednet/trustednet.docs/ajax.php?command=content&id=1'
+file1.url = 'http://localhost:3000/resources/file1.txt'
 file1.id = 1;
 
 var file2 = {};
 file2.name = 'filename2.txt';
-file2.sys_name = 'filename2.txt.p7s';
-file2.url = 'https://bitrix.tsumo.org/bitrix/components/trustednet/trustednet.docs/ajax.php?command=content&id=2'
+file2.url = 'http://localhost:3000/resources/file2.txt'
 file2.id = 2;
 
 var file3 = {};
 file3.name = 'filename3.txt';
-file3.sys_name = 'filename3.txt';
-file3.url = 'https://bitrix.tsumo.org/bitrix/components/trustednet/trustednet.docs/ajax.php?command=content&id=3'
+file3.url = 'http://localhost:3000/resources/file3.txt'
 file3.id = 3;
 
 var signRequest = {};
@@ -32,16 +29,12 @@ signRequest.params.uploader = 'https://bitrix.tsumo.org/bitrix/components/truste
 signRequest.params.cancel = 'https://bitrix.tsumo.org/bitrix/components/trustednet/trustednet.sign/ajax.php?command=updateStatus&status=2';
 signRequest.params.error = 'https://bitrix.tsumo.org/bitrix/components/trustednet/trustednet.sign/ajax.php?command=updateStatus&status=3';
 
-socket.on('files signed', function (data) {
-	console.log('файл подписан', data);
+socket.on('signed', function (data) {
+	console.log('file signed - ', data);
 });
 
-socket.on('file saved', function () {
-	console.log("file saved");
-});
-
-socket.on('signature verified', function (data) {
-	console.log('информация о подписи', data);
+socket.on('verified', function (data) {
+	console.log('signature information', data);
 });
 
 formsign.onsubmit = function(ev) {
--- demo/demo-socket.io/server.js
+++ demo/demo-socket.io/server.js
@@ -1,44 +1,56 @@
 "use strict";
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
-const https = require('https');
-const fs = require('fs');
+const http = require("http");
+const https = require("https");
+const fs = require("fs");
 
-const privateKey  = fs.readFileSync('ssl/key.pem', 'utf8');
-const certificate = fs.readFileSync('ssl/cert.pem', 'utf8');
+const privateKey = fs.readFileSync("ssl/key.pem", "utf8");
+const certificate = fs.readFileSync("ssl/cert.pem", "utf8");
 
-const credentials = {key: privateKey, cert: certificate};
+const credentials = { key: privateKey, cert: certificate };
 
 const httpServer = http.Server(app);
 const httpsServer = https.Server(credentials, app);
 
-const io = require('socket.io')(httpServer);
+const io = require("socket.io")(httpServer);
 
-app.get('/', (req, res, next) => {
-	return res.sendFile(__dirname + '/client/index.html');
+app.get("/", (req, res, next) => {
+  return res.sendFile(__dirname + "/client/index.html");
 });
 
-app.get('/app.js', (req, res, next) => {
-	return res.sendFile(__dirname + '/client/dist/app.js');
+app.get("/app.js", (req, res, next) => {
+  return res.sendFile(__dirname + "/client/dist/app.js");
 });
 
-app.get('/socket.io.js', (req, res, next) => {
-	return res.sendFile(__dirname + '/node_modules/socket.io-client/dist/socket.io.js');
+app.get("/socket.io.js", (req, res, next) => {
+  return res.sendFile(__dirname + "/node_modules/socket.io-client/dist/socket.io.js");
 });
 
-app.get('/socket.io-file-client.js', (req, res, next) => {
-	return res.sendFile(__dirname + '/node_modules/socket.io-file-client/socket.io-file-client.js');
+app.get("/socket.io-file-client.js", (req, res, next) => {
+  return res.sendFile(__dirname + "/node_modules/socket.io-file-client/socket.io-file-client.js");
 });
 
-app.get('/socket.io-stream.js', (req, res, next) => {
-	return res.sendFile(__dirname + '/node_modules/socket.io-stream/socket.io-stream.js');
+app.get("/socket.io-stream.js", (req, res, next) => {
+  return res.sendFile(__dirname + "/node_modules/socket.io-stream/socket.io-stream.js");
+});
+
+app.get("/resources/file1.txt", (req, res, next) => {
+  return res.sendFile(__dirname + "/resources/file1.txt");
+});
+
+app.get("/resources/file2.txt", (req, res, next) => {
+  return res.sendFile(__dirname + "/resources/file2.txt");
+});
+
+app.get("/resources/file3.txt", (req, res, next) => {
+  return res.sendFile(__dirname + "/resources/file2.txt");
 });
 
 httpServer.listen(3000, () => {
-	console.log('Server listening on port 3000');
+  console.log("Server listening on port 3000");
 });
 
 httpsServer.listen(4000, () => {
-	console.log('SSL server listening on port 4000');
+  console.log("SSL server listening on port 4000");
 });
