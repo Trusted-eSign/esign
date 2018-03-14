faf0d21fdaec9a7ab3a3a4b213a6e7f84ffcef00
Aleksandr Gavrilov <alg@digt.ru>
2018-03-14 10:44:09 +0300

    add install certificate to firefox on win
    use certutil

--- app/resources/ssl/addCA.win.bat
+++ app/resources/ssl/addCA.win.bat
@@ -1,4 +1,24 @@
 @echo off
 
+setlocal
+
 @echo Installing "CryptoARM Local CA" certificate
 certutil -addstore -user root %~dp0\root.pem
+
+REM ####Current user
+setlocal ENABLEDELAYEDEXPANSION
+set certpath=%~dp0root.pem
+set certName=CryptoARM Local CA
+
+FOR /D %%P IN ("%appdata%\Mozilla\Firefox\Profiles\*") DO (
+"%~dp0certutil\win32\certutil.exe" -D -n "!certName!" -d sql:"%%P"
+"%~dp0certutil\win32\certutil.exe" -D -n "!certName!" -d dbm:"%%P"
+
+"%~dp0certutil\win32\certutil.exe" -A -n "!certName!" -i "%certpath%" -t "C,c,c", -d sql:"%%P"
+"%~dp0certutil\win32\certutil.exe" -A -n "!certName!" -i "%certpath%" -t "C,c,c", -d dbm:"%%P"
+)
+setlocal DISABLEDELAYEDEXPANSION
+
+
+REM #### eof
+exit /B 0
