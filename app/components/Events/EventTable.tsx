b2f15d6ccdee9916d7905763b595e1142dde0f5b
Aleksandr Gavrilov <alg@digt.ru>
2018-06-15 15:35:38 +0300

    add search result navigation

--- app/components/Events/EventTable.tsx
+++ app/components/Events/EventTable.tsx
@@ -26,7 +26,7 @@ interface IEventTableDispatch {
 interface IEventTableState {
   disableHeader: boolean;
   foundEvents: number[];
-  scrollToIndex: number | undefined;
+  scrollToIndex: number;
   sortBy: string;
   sortDirection: TSortDirection;
   sortedList: any;
@@ -48,7 +48,7 @@ class EventTable extends React.Component<IEventTableProps & IEventTableDispatch,
     this.state = {
       disableHeader: false,
       foundEvents: [],
-      scrollToIndex: undefined,
+      scrollToIndex: 0,
       sortBy,
       sortDirection,
       sortedList,
@@ -81,8 +81,8 @@ class EventTable extends React.Component<IEventTableProps & IEventTableDispatch,
 
   render() {
     const { locale } = this.context;
-    const { isLoading } = this.props;
-    const { disableHeader, scrollToIndex, sortBy, sortDirection, sortedList } = this.state;
+    const { isLoading, searchValue } = this.props;
+    const { disableHeader, foundEvents, scrollToIndex, sortBy, sortDirection, sortedList } = this.state;
 
     if (isLoading) {
       return <ProgressBars />;
@@ -91,103 +91,142 @@ class EventTable extends React.Component<IEventTableProps & IEventTableDispatch,
     const rowGetter = ({ index }: { index: number }) => this.getDatum(this.state.sortedList, index);
 
     return (
-      <Table
-        ref="Table"
-        disableHeader={disableHeader}
-        height={430}
-        width={780}
-        headerHeight={30}
-        noRowsRenderer={this.noRowsRenderer}
-        headerClassName={"headerColumn"}
-        rowHeight={45}
-        rowClassName={this.rowClassName}
-        overscanRowCount={5}
-        rowGetter={rowGetter}
-        rowCount={sortedList.size}
-        scrollToIndex={scrollToIndex}
-        sort={this.sort}
-        sortBy={sortBy}
-        sortDirection={sortDirection}
-      >
-        <Column
-          cellRenderer={({ cellData }) => {
-            return (new Date(cellData)).toLocaleDateString(locale, {
-              day: "numeric",
-              hour: "numeric",
-              minute: "numeric",
-              month: "numeric",
-              year: "numeric",
-            });
-          }}
-          dataKey="timestamp"
-          disableSort={false}
-          width={130}
-          label="Дата и время"
-        />
-        <Column
-          dataKey="operation"
-          disableSort={false}
-          headerRenderer={this.headerRenderer}
-          width={180}
-          label="Операция"
-        />
-        <Column
-          dataKey="userName"
-          disableSort={false}
-          headerRenderer={this.headerRenderer}
-          width={120}
-          label="Пользователь"
-        />
-        <Column
-          cellRenderer={({ cellData }) => {
-            return (
-              <div className="row nobottom">
-                <div className="col s12">
-                  <div className="truncate">{cellData.in}</div>
-                  <div className="truncate" style={{ opacity: .6 }}> -> {cellData.out}</div>
-                </div>
-              </div>
-            );
-          }}
-          dataKey="operationObject"
-          disableSort
-          width={250}
-          label="Объект операции"
-        />
-        <Column
-          cellRenderer={({ cellData }) => {
-            let msg;
-            let levelStyle;
-
-            switch (cellData) {
-              case "info":
-                msg = "Успех";
-                levelStyle = "icon_ok";
-                break;
-              case "error":
-                msg = "Ошибка";
-                levelStyle = "icon_fail";
-                break;
-            }
-
-            return (
-              <div className="row nobottom">
-                <div className="valign-wrapper">
+      <div>
+        <Table
+          ref="Table"
+          disableHeader={disableHeader}
+          height={475}
+          width={780}
+          headerHeight={30}
+          noRowsRenderer={this.noRowsRenderer}
+          headerClassName={"headerColumn"}
+          rowHeight={45}
+          rowClassName={this.rowClassName}
+          overscanRowCount={5}
+          rowGetter={rowGetter}
+          rowCount={sortedList.size}
+          scrollToIndex={scrollToIndex}
+          sort={this.sort}
+          sortBy={sortBy}
+          sortDirection={sortDirection}
+        >
+          <Column
+            cellRenderer={({ cellData }) => {
+              return (new Date(cellData)).toLocaleDateString(locale, {
+                day: "numeric",
+                hour: "numeric",
+                minute: "numeric",
+                month: "numeric",
+                year: "numeric",
+              });
+            }}
+            dataKey="timestamp"
+            disableSort={false}
+            width={130}
+            label="Дата и время"
+          />
+          <Column
+            dataKey="operation"
+            disableSort={false}
+            headerRenderer={this.headerRenderer}
+            width={180}
+            label="Операция"
+          />
+          <Column
+            dataKey="userName"
+            disableSort={false}
+            headerRenderer={this.headerRenderer}
+            width={120}
+            label="Пользователь"
+          />
+          <Column
+            cellRenderer={({ cellData }) => {
+              return (
+                <div className="row nobottom">
                   <div className="col s12">
-                    <p>{msg}</p>
+                    <div className="truncate">{cellData.in}</div>
+                    <div className="truncate" style={{ opacity: .6 }}> -> {cellData.out}</div>
                   </div>
                 </div>
-              </div>
-            );
-          }}
-          dataKey="level"
-          disableSort={false}
-          headerRenderer={this.headerRenderer}
-          width={100}
-          label="Статус"
-        />
-      </Table>
+              );
+            }}
+            dataKey="operationObject"
+            disableSort
+            width={250}
+            label="Объект операции"
+          />
+          <Column
+            cellRenderer={({ cellData }) => {
+              let msg;
+              let levelStyle;
+
+              switch (cellData) {
+                case "info":
+                  msg = "Успех";
+                  levelStyle = "icon_ok";
+                  break;
+                case "error":
+                  msg = "Ошибка";
+                  levelStyle = "icon_fail";
+                  break;
+              }
+
+              return (
+                <div className="row nobottom">
+                  <div className="valign-wrapper">
+                    <div className="col s12">
+                      <p>{msg}</p>
+                    </div>
+                  </div>
+                </div>
+              );
+            }}
+            dataKey="level"
+            disableSort={false}
+            headerRenderer={this.headerRenderer}
+            width={100}
+            label="Статус"
+          />
+        </Table>
+        {searchValue ?
+          <div className="card navigationToolbar valign-wrapper">
+            <i className="small material-icons cryptoarm-blue" onClick={this.handleScrollToFirstOfFoud}>first_page</i>
+            <i className="small material-icons cryptoarm-blue" onClick={this.handleScrollToBefore}>navigate_before</i>
+            {foundEvents.indexOf(scrollToIndex) + 1}/{foundEvents.length}
+            <i className="small material-icons cryptoarm-blue" onClick={this.handleScrollToNext}>navigate_next</i>
+            <i className="small material-icons cryptoarm-blue" onClick={this.handleScrollToLastOfFoud}>last_page</i>
+          </div> :
+          null}
+      </div>
     );
+  }
+
+  handleScrollToBefore = () => {
+    const { foundEvents, scrollToIndex } = this.state;
+
+    if (foundEvents.indexOf(scrollToIndex) - 1 >= 0) {
+      this.scrollToRow(foundEvents[foundEvents.indexOf(scrollToIndex) - 1]);
+    }
+  }
+
+  handleScrollToNext = () => {
+    const { foundEvents, scrollToIndex } = this.state;
+
+    if (foundEvents.indexOf(scrollToIndex) + 1 < foundEvents.length) {
+      this.scrollToRow(foundEvents[foundEvents.indexOf(scrollToIndex) + 1]);
+    }
+  }
+
+  handleScrollToFirstOfFoud = () => {
+    const { foundEvents } = this.state;
+
+    this.scrollToRow(foundEvents[0]);
+  }
+
+  handleScrollToLastOfFoud = () => {
+    const { foundEvents } = this.state;
+
+    this.scrollToRow(foundEvents[foundEvents.length - 1]);
   }
 
   getDatum = (list: any, index: number) => {
