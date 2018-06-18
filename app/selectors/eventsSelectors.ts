d0bfaaf3f4695bfeadae40d3cf73a21dd00596b1
Aleksandr Gavrilov <alg@digt.ru>
2018-06-18 15:12:06 +0300

    add operation type to selector

--- app/selectors/eventsSelectors.ts
+++ app/selectors/eventsSelectors.ts
@@ -1,10 +1,11 @@
 import { createSelector } from "reselect";
+import { CERTIFICATE_GENERATION, CERTIFICATE_IMPORT, DECRYPT, ENCRYPT, SIGN } from "../constants";
 
 export const eventsGetter = (state) => state.events.entities;
 export const filtersGetter = (state) => state.filters;
 
 export const filteredEventsSelector = createSelector(eventsGetter, filtersGetter, (events, filters) => {
-  const { dateFrom, dateTo, level, operationObjectIn, operationObjectOut, userName } = filters;
+  const { dateFrom, dateTo, level,operations, operationObjectIn, operationObjectOut, userName } = filters;
 
   return events.filter((event: any) => {
     return event.userName.match(userName) &&
@@ -12,6 +13,13 @@ export const filteredEventsSelector = createSelector(eventsGetter, filtersGetter
       event.operationObject.out.match(operationObjectOut) &&
       (level === "all" ? true : event.level.match(level)) &&
       (dateFrom ? (new Date(event.timestamp)).getTime() >= (new Date(dateFrom)).getTime() : true) &&
-      (dateTo ? (new Date(event.timestamp)).getTime() <= (new Date(dateTo)).getTime() : true);
+      (dateTo ? (new Date(event.timestamp)).getTime() <= (new Date(dateTo)).getTime() : true) &&
+      (
+        operations[SIGN] && event.operation === "Подпись" ||
+        operations[ENCRYPT] && event.operation === "Шифрование" ||
+        operations[DECRYPT] && event.operation === "Расшифрование" ||
+        operations[CERTIFICATE_GENERATION] && event.operation === "Генерация сертификата" ||
+        operations[CERTIFICATE_IMPORT] && event.operation === "Импорт сертификата"
+      );
   });
 });
