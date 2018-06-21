92d2f4d6e643c3c0c5acfa92db10f62917909412
Aleksandr Gavrilov <alg@digt.ru>
2018-06-21 13:18:59 +0300

    add check default filters

--- app/reducer/filters.ts
+++ app/reducer/filters.ts
@@ -9,7 +9,7 @@ const defaultFilters = {
   dateFrom: undefined,
   dateTo: undefined,
   isDefaultFilters: true,
-  level: "",
+  level: "all",
   operationObjectIn: "",
   operationObjectOut: "",
   operations: {
@@ -31,18 +31,18 @@ export default (filters = defaultFilters, action) => {
       return { ...filters, searchValue: payload.searchValue };
 
     case CHANGE_FILTER_USER_NAME:
-      return { ...filters, userName: payload.userName, isDefaultFilters: false };
+      return { ...filters, userName: payload.userName, isDefaultFilters: checkDefaultFilters({ ...filters, userName: payload.userName }) };
 
     case CHANGE_FILTER_IN_OPERATION_OBJECT:
-      return { ...filters, operationObjectIn: payload.operationObjectIn, isDefaultFilters: false };
+      return { ...filters, operationObjectIn: payload.operationObjectIn, isDefaultFilters: checkDefaultFilters({ ...filters, operationObjectIn: payload.operationObjectIn }) };
 
     case CHANGE_FILTER_OUT_OPERATION_OBJECT:
-      return { ...filters, operationObjectOut: payload.operationObjectOut, isDefaultFilters: false };
+      return { ...filters, operationObjectOut: payload.operationObjectOut, isDefaultFilters: checkDefaultFilters({ ...filters, operationObjectOut: payload.operationObjectOut }) };
 
     case CHANGE_FILTER_OPERATION_TYPE:
       return {
         ...filters,
-        isDefaultFilters: false,
+        isDefaultFilters: checkDefaultFilters({ ...filters, operations: { ...filters.operations, [payload.type]: payload.value } }),
         operations: {
           ...filters.operations,
           [payload.type]: payload.value,
@@ -50,13 +50,13 @@ export default (filters = defaultFilters, action) => {
       };
 
     case CHANGE_FILTER_DATE_FROM:
-      return { ...filters, dateFrom: payload.dateFrom, isDefaultFilters: false };
+      return { ...filters, dateFrom: payload.dateFrom, isDefaultFilters: checkDefaultFilters({ ...filters, dateFrom: payload.dateFrom }) };
 
     case CHANGE_FILTER_DATE_TO:
-      return { ...filters, dateTo: payload.dateTo, isDefaultFilters: false };
+      return { ...filters, dateTo: payload.dateTo, isDefaultFilters: checkDefaultFilters({ ...filters, dateTo: payload.dateTo }) };
 
     case CHANGE_FILTER_LEVEL:
-      return { ...filters, level: payload.level, isDefaultFilters: false };
+      return { ...filters, level: payload.level, isDefaultFilters: checkDefaultFilters({ ...filters, level: payload.level }) };
 
     case RESET_EVENTS_FILTERS:
       return {
@@ -79,4 +79,24 @@ export default (filters = defaultFilters, action) => {
   }
 
   return filters;
+};
+
+const checkDefaultFilters = (filters: any) => {
+  if (
+    defaultFilters.dateFrom === filters.dateFrom &&
+    defaultFilters.dateTo === filters.dateTo &&
+    defaultFilters.level === filters.level &&
+    defaultFilters.operationObjectIn === filters.operationObjectIn &&
+    defaultFilters.operationObjectOut === filters.operationObjectOut &&
+    defaultFilters.operations.CERTIFICATE_GENERATION === filters.operations.CERTIFICATE_GENERATION &&
+    defaultFilters.operations.CERTIFICATE_IMPORT === filters.operations.CERTIFICATE_IMPORT &&
+    defaultFilters.operations.DECRYPT === filters.operations.DECRYPT &&
+    defaultFilters.operations.ENCRYPT === filters.operations.ENCRYPT &&
+    defaultFilters.operations.SIGN === filters.operations.SIGN &&
+    defaultFilters.userName === filters.userName
+  ) {
+    return true;
+  }
+
+  return false;
 };
