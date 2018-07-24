0c87855653a38891f9ecdc67c1d5249d7beba812
Aleksandr Gavrilov <alg@digt.ru>
2018-07-24 10:47:15 +0300

    add red border

--- app/components/Documents/DocumentsWindow.tsx
+++ app/components/Documents/DocumentsWindow.tsx
@@ -63,7 +63,7 @@ class DocumentsWindow extends React.Component<IDocumentsWindowProps, IDocumentsW
       outDuration: 225,
     });
 
-    $(document).ready(function() {
+    $(document).ready(function () {
       $(".tooltipped").tooltip();
     });
   }
@@ -79,96 +79,98 @@ class DocumentsWindow extends React.Component<IDocumentsWindowProps, IDocumentsW
     const disabledClass = documents.length ? "" : "disabled_docs";
 
     return (
-      <div className="row">
-        <div className="row halfbottom" />
+      <div className="content">
+        <div className="row">
+          <div className="row halfbottom" />
 
-        <div className="col s10">
-          <div className="input-field input-field-csr col s12 border_element find_box">
-            <i className="material-icons prefix">search</i>
-            <input
-              id="search"
-              type="search"
-              placeholder={localize("EventsTable.search_in_doclist", locale)}
-              value={this.state.searchValue}
-              onChange={this.handleSearchValueChange} />
-            <i className="material-icons close" onClick={() => this.setState({ searchValue: "" })} style={this.state.searchValue ? { color: "#444" } : {}}>close</i>
-          </div>
-        </div>
-        <div className="col s1">
-          <a className={"btn-small waves-effect waves-light"} onClick={this.handleShowModalFilterDocuments}>
-            <i className={"material-icons " + classDefaultFilters}>filter_list</i>
-          </a>
-        </div>
-        <div className="col s1">
-          <a className={"nav-small-btn waves-effect waves-light"} data-activates="dropdown-btn-for-documents" style={{ margin: 0 }}>
-            <i className="nav-small-icon material-icons context_menu">more_vert</i>
-          </a>
-          <ul id="dropdown-btn-for-documents" className="dropdown-content">
-            <li><a onClick={this.handleReloadDocuments}>{localize("Common.update", locale)}</a></li>
-            <li><a onClick={this.handleSelectAllDocuments}>{localize("Documents.selected_all", locale)}</a></li>
-            <li><a onClick={this.handleOpenDocumentsFolder}>{localize("Documents.go_to_documents_folder", locale)}</a></li>
-          </ul>
-        </div>
-        <div className="col s12">
-          <div className="row halfbottom">
-            <div className="col s10prt">
-              <a className={`waves-effect waves-light  ${this.checkEnableOperationButton(SIGN) ? "" : "disabled_docs"}`}
-                data-position="bottom"
-                onClick={this.handleClickSign}>
-                <div className="row docmenu"><i className="material-icons docmenu_sign"></i></div>
-                <div className="row docmenu">{localize("Documents.docmenu_sign", locale)}</div>
-              </a>
-            </div>
-            <div className="col s10prt">
-              <a className={`waves-effect waves-light  ${this.checkEnableOperationButton(VERIFY) ? "" : "disabled_docs"}`}
-                data-position="bottom"
-                data-tooltip={localize("Sign.sign_and_verify", locale)}
-                onClick={this.handleClickSign}>
-                <div className="row docmenu"><i className="material-icons docmenu_verifysign"></i></div>
-                <div className="row docmenu">{localize("Documents.docmenu_verifysign", locale)}</div>
-              </a>
-            </div>
-            <div className="col s10prt">
-              <a className={`waves-effect waves-light ${this.checkEnableOperationButton(UNSIGN) ? "" : "disabled_docs"}`} data-position="bottom"
-                onClick={this.handleClickSign}>
-                <div className="row docmenu"><i className="material-icons docmenu_removesign"></i></div>
-                <div className="row docmenu">{localize("Documents.docmenu_removesign", locale)}</div>
-              </a>
-            </div>
-            <div className="col s10prt">
-              <a className={`waves-effect waves-light ${this.checkEnableOperationButton(ENCRYPT) ? "" : "disabled_docs"}`}
-                data-position="bottom" onClick={this.handleClickEncrypt}>
-                <div className="row docmenu"><i className="material-icons docmenu_encrypt"></i></div>
-                <div className="row docmenu">{localize("Documents.docmenu_enctypt", locale)}</div>
-              </a>
-            </div>
-            <div className="col s10prt">
-              <a className={`waves-effect waves-light ${this.checkEnableOperationButton(DECRYPT) ? "" : "disabled_docs"}`} data-position="bottom"
-                onClick={this.handleClickEncrypt} >
-                <div className="row docmenu"><i className="material-icons docmenu_decrypt"></i></div>
-                <div className="row docmenu">{localize("Documents.docmenu_dectypt", locale)}</div>
-              </a>
-            </div>
-            <div className="col s10prt">
-              <a className={`waves-effect waves-light ${disabledClass}`} data-position="bottom"
-                onClick={this.handleArhiveDocuments} >
-                <div className="row docmenu"><i className="material-icons docmenu_arhiver"></i></div>
-                <div className="row docmenu">{localize("Documents.docmenu_arhiver", locale)}</div>
-              </a>
-            </div>
-            <div className="col s1">
-              <a className={`waves-effect waves-light ${disabledClass}`} data-position="bottom"
-                onClick={this.handleClickDelete} >
-                <div className="row docmenu"><i className="material-icons docmenu_remove"></i></div>
-                <div className="row docmenu">{localize("Documents.docmenu_remove", locale)}</div>
-              </a>
+          <div className="col s10">
+            <div className="input-field input-field-csr col s12 border_element find_box">
+              <i className="material-icons prefix">search</i>
+              <input
+                id="search"
+                type="search"
+                placeholder={localize("EventsTable.search_in_doclist", locale)}
+                value={this.state.searchValue}
+                onChange={this.handleSearchValueChange} />
+              <i className="material-icons close" onClick={() => this.setState({ searchValue: "" })} style={this.state.searchValue ? { color: "#444" } : {}}>close</i>
             </div>
           </div>
+          <div className="col s1">
+            <a className={"btn-small waves-effect waves-light"} onClick={this.handleShowModalFilterDocuments}>
+              <i className={"material-icons " + classDefaultFilters}>filter_list</i>
+            </a>
+          </div>
+          <div className="col s1">
+            <a className={"nav-small-btn waves-effect waves-light"} data-activates="dropdown-btn-for-documents" style={{ margin: 0 }}>
+              <i className="nav-small-icon material-icons context_menu">more_vert</i>
+            </a>
+            <ul id="dropdown-btn-for-documents" className="dropdown-content">
+              <li><a onClick={this.handleReloadDocuments}>{localize("Common.update", locale)}</a></li>
+              <li><a onClick={this.handleSelectAllDocuments}>{localize("Documents.selected_all", locale)}</a></li>
+              <li><a onClick={this.handleOpenDocumentsFolder}>{localize("Documents.go_to_documents_folder", locale)}</a></li>
+            </ul>
+          </div>
+          <div className="col s12">
+            <div className="row halfbottom">
+              <div className="col s10prt">
+                <a className={`waves-effect waves-light  ${this.checkEnableOperationButton(SIGN) ? "" : "disabled_docs"}`}
+                  data-position="bottom"
+                  onClick={this.handleClickSign}>
+                  <div className="row docmenu"><i className="material-icons docmenu_sign"></i></div>
+                  <div className="row docmenu">{localize("Documents.docmenu_sign", locale)}</div>
+                </a>
+              </div>
+              <div className="col s10prt">
+                <a className={`waves-effect waves-light  ${this.checkEnableOperationButton(VERIFY) ? "" : "disabled_docs"}`}
+                  data-position="bottom"
+                  data-tooltip={localize("Sign.sign_and_verify", locale)}
+                  onClick={this.handleClickSign}>
+                  <div className="row docmenu"><i className="material-icons docmenu_verifysign"></i></div>
+                  <div className="row docmenu">{localize("Documents.docmenu_verifysign", locale)}</div>
+                </a>
+              </div>
+              <div className="col s10prt">
+                <a className={`waves-effect waves-light ${this.checkEnableOperationButton(UNSIGN) ? "" : "disabled_docs"}`} data-position="bottom"
+                  onClick={this.handleClickSign}>
+                  <div className="row docmenu"><i className="material-icons docmenu_removesign"></i></div>
+                  <div className="row docmenu">{localize("Documents.docmenu_removesign", locale)}</div>
+                </a>
+              </div>
+              <div className="col s10prt">
+                <a className={`waves-effect waves-light ${this.checkEnableOperationButton(ENCRYPT) ? "" : "disabled_docs"}`}
+                  data-position="bottom" onClick={this.handleClickEncrypt}>
+                  <div className="row docmenu"><i className="material-icons docmenu_encrypt"></i></div>
+                  <div className="row docmenu">{localize("Documents.docmenu_enctypt", locale)}</div>
+                </a>
+              </div>
+              <div className="col s10prt">
+                <a className={`waves-effect waves-light ${this.checkEnableOperationButton(DECRYPT) ? "" : "disabled_docs"}`} data-position="bottom"
+                  onClick={this.handleClickEncrypt} >
+                  <div className="row docmenu"><i className="material-icons docmenu_decrypt"></i></div>
+                  <div className="row docmenu">{localize("Documents.docmenu_dectypt", locale)}</div>
+                </a>
+              </div>
+              <div className="col s10prt">
+                <a className={`waves-effect waves-light ${disabledClass}`} data-position="bottom"
+                  onClick={this.handleArhiveDocuments} >
+                  <div className="row docmenu"><i className="material-icons docmenu_arhiver"></i></div>
+                  <div className="row docmenu">{localize("Documents.docmenu_arhiver", locale)}</div>
+                </a>
+              </div>
+              <div className="col s1">
+                <a className={`waves-effect waves-light ${disabledClass}`} data-position="bottom"
+                  onClick={this.handleClickDelete} >
+                  <div className="row docmenu"><i className="material-icons docmenu_remove"></i></div>
+                  <div className="row docmenu">{localize("Documents.docmenu_remove", locale)}</div>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col s12">
+            <DocumentsTable searchValue={this.state.searchValue} />
+          </div>
+          {this.showModalFilterDocuments()}
         </div>
-        <div className="col s12">
-          <DocumentsTable searchValue={this.state.searchValue} />
-        </div>
-        {this.showModalFilterDocuments()}
       </div>
     );
   }
