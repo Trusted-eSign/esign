c1f709e3962615085caf21ce463a9155060dbd21
Aleksandr Gavrilov <alg@digt.ru>
2018-10-09 14:07:30 +0300

    add simple certificates virtualized List

--- app/components/Certificate/CertificateList.tsx
+++ app/components/Certificate/CertificateList.tsx
@@ -1,7 +1,9 @@
 import PropTypes from "prop-types";
 import React from "react";
 import { connect } from "react-redux";
+import { AutoSizer, List } from "react-virtualized";
 import { loadAllCertificates, verifyCertificate } from "../../AC";
+import { ENCRYPT, SIGN } from "../../constants";
 import accordion from "../../decorators/accordion";
 import { filteredCertificatesSelector } from "../../selectors";
 import ProgressBars from "../ProgressBars";
@@ -12,18 +14,30 @@ interface ICertificateListProps {
   certificates: any;
   isLoaded: boolean;
   isLoading: boolean;
+  operation: string;
   loadAllCertificates: () => void;
   verifyCertificate: (id: number) => void;
 }
 
-let scrollTimer: NodeJS.Timer | null;
-let lastScrollFireTime = 0;
+// let scrollTimer: NodeJS.Timer | null;
+// let lastScrollFireTime = 0;
 
-class CertificateList extends React.Component<ICertificateListProps, {}> {
+const HEIGHT_MODAL = 356;
+const HEIGHT_FULL = 432;
+
+class CertificateList extends React.Component<ICertificateListProps, any> {
   static contextTypes = {
     locale: PropTypes.string,
     localize: PropTypes.func,
   };
+
+  constructor(props: ICertificateListProps) {
+    super(props);
+
+    this.state = ({
+      activeSection: "my",
+    });
+  }
 
   componentDidMount() {
     // tslint:disable-next-line:no-shadowed-variable
@@ -35,53 +49,55 @@ class CertificateList extends React.Component<ICertificateListProps, {}> {
 
     $(".collapsible").collapsible();
 
-    const addCerts = $(".add-certs");
+    // const addCerts = $(".add-certs");
 
-    if (addCerts.length) {
-      addCerts[0].addEventListener("scroll", this.fireOnScroll);
-    }
+    // if (addCerts.length) {
+    //   addCerts[0].addEventListener("scroll", this.fireOnScroll);
+    // }
 
-    this.processScroll();
+    // this.processScroll();
   }
 
-  fireOnScroll = () => {
-    const minScrollTime = 1000;
-    const now = new Date().getTime();
-    const self = this;
+  // fireOnScroll = () => {
+  //   const minScrollTime = 1000;
+  //   const now = new Date().getTime();
+  //   const self = this;
 
-    if (!scrollTimer) {
-      if (now - lastScrollFireTime > (3 * minScrollTime)) {
-        lastScrollFireTime = now;
-      }
+  //   if (!scrollTimer) {
+  //     if (now - lastScrollFireTime > (3 * minScrollTime)) {
+  //       lastScrollFireTime = now;
+  //     }
 
-      scrollTimer = setTimeout(function() {
-        scrollTimer = null;
-        lastScrollFireTime = new Date().getTime();
-        self.processScroll();
-      }, minScrollTime);
-    }
-  }
+  //     scrollTimer = setTimeout(function() {
+  //       scrollTimer = null;
+  //       lastScrollFireTime = new Date().getTime();
+  //       self.processScroll();
+  //     }, minScrollTime);
+  //   }
+  // }
 
-  processScroll = () => {
-    // tslint:disable-next-line:no-shadowed-variable
-    const { certificates, verifyCertificate } = this.props;
+  // processScroll = () => {
+  //   // tslint:disable-next-line:no-shadowed-variable
+  //   const { certificates, verifyCertificate } = this.props;
 
-    certificates.forEach((cert: any) => {
-      let visible = false;
+  //   certificates.forEach((cert: any) => {
+  //     let visible = false;
 
-      if ($(`#${cert.id}:visible`).length > 0) {
-        visible = $(`#${cert.id}:visible`).visible();
-      }
+  //     if ($(`#${cert.id}:visible`).length > 0) {
+  //       visible = $(`#${cert.id}:visible`).visible();
+  //     }
 
-      if (visible) {
-        if (!cert.verified) {
-          verifyCertificate(cert.id);
-        }
-      }
-    });
-  }
+  //     if (visible) {
+  //       if (!cert.verified) {
+  //         verifyCertificate(cert.id);
+  //       }
+  //     }
+  //   });
+  // }
 
   getCollapsibleElement(head: string, name: string, elements: object[], active: boolean = false) {
+    const { certificates, activeCert, selectedCert, operation, toggleOpenItem, isItemOpened, isLoading } = this.props;
+
     if (!elements || elements.length === 0) {
       return null;
     }
@@ -90,14 +106,60 @@ class CertificateList extends React.Component<ICertificateListProps, {}> {
 
     return (
       <li>
-        <div className={`collapsible-header color ${activeSection}`} onClick={this.processScroll}>
+        <div className={`collapsible-header color ${activeSection}`} onClick={() => this.setState({activeSection: name})}>
           <i className={`material-icons left ${name}`}>
           </i>
           {head}
         </div>
-        <div className="collapsible-body">{elements}</div>
+        <div className="collapsible-body">
+          <List
+            height={this.getListHeight(elements)}
+            overscanRowCount={1}
+            rowCount={elements.length}
+            rowHeight={45}
+            rowRenderer={({ index, key, style }) => {
+
+              if (!elements.length || this.state.activeSection !== name) {
+                return null;
+              }
+
+              const cert = elements[index];
+
+              return (
+                <ul
+                  key={key}
+                  style={style}
+                >
+                  <CertificateListItem
+                    key={cert.id}
+                    cert={cert}
+                    chooseCert={() => activeCert(cert)}
+                    operation={operation}
+                    selectedCert={() => selectedCert(cert)}
+                    isOpen={isItemOpened(cert.id.toString())}
+                    toggleOpen={toggleOpenItem(cert.id.toString())}
+                    style={style} />;
+                </ul>
+              );
+            }}
+            width={369}
+          />
+        </div>
       </li>
     );
+  }
+
+  getListHeight = () => {
+    const { operation } = this.props;
+
+    switch (operation) {
+      case "sign":
+      case "encrypt":
+        return HEIGHT_MODAL;
+
+      default:
+        return HEIGHT_FULL;
+    }
   }
 
   render() {
@@ -116,30 +178,30 @@ class CertificateList extends React.Component<ICertificateListProps, {}> {
     const request: object[] = [];
 
     certificates.forEach((cert) => {
-      const element = (
-        <CertificateListItem
-          key={cert.id}
-          cert={cert}
-          chooseCert={() => activeCert(cert)}
-          operation={operation}
-          selectedCert={() => selectedCert(cert)}
-          isOpen={isItemOpened(cert.id.toString())}
-          toggleOpen={toggleOpenItem(cert.id.toString())} />
-      );
+      // const element = (
+      //   <CertificateListItem
+      //     key={cert.id}
+      //     cert={cert}
+      //     chooseCert={() => activeCert(cert)}
+      //     operation={operation}
+      //     selectedCert={() => selectedCert(cert)}
+      //     isOpen={isItemOpened(cert.id.toString())}
+      //     toggleOpen={toggleOpenItem(cert.id.toString())} />
+      // );
 
       switch (cert.category) {
         case "MY":
-          return my.push(element);
+          return my.push(cert);
         case "ROOT":
-          return root.push(element);
+          return root.push(cert);
         case "CA":
-          return intermediate.push(element);
+          return intermediate.push(cert);
         case "AddressBook":
-          return other.push(element);
+          return other.push(cert);
         case "TOKEN":
-          return token.push(element);
+          return token.push(cert);
         case "Request":
-          return request.push(element);
+          return request.push(cert);
       }
     });
 
