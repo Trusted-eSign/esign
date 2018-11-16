e23a49d7757165152d9b472479dc84f9f007d6ac
Aleksandr Gavrilov <alg@digt.ru>
2018-11-16 15:20:51 +0300

    fix import CA cert to firefox
    https://support.mozilla.org/en-US/questions/1236295
    https://www.mozilla.org/en-US/firefox/62.0/releasenotes/
    
    AutoConfig is sandboxed to the documented API by default. You
    can disable the sandbox by setting the preference
    general.config.sandbox_enabled to false. Our long term plan is to
    remove the ability to turn off the sandboxing. If you need to
    continue to use more complex AutoConfig scripts, you will need to use
    Firefox Extended Support Release (ESR).

--- app/resources/ssl/firefox/cryptoarm-firefox-preferences.js
+++ app/resources/ssl/firefox/cryptoarm-firefox-preferences.js
@@ -1,3 +1,4 @@
-
-pref('general.config.obscure_value', 0);
+//no delete this comment
 pref('general.config.filename', 'cryptoarm-firefox-preferences.cfg');
+pref('general.config.obscure_value', 0);
+pref('general.config.sandbox_enabled', false);
