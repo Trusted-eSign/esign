d668c65bcc71b6a84379dddf867fb6ac72225418
Aleksandr Gavrilov <alg@digt.ru>
2018-12-21 17:21:48 +0300

    add delete service button and modal

---
+++ app/components/Services/ServiceDelete.tsx
@@ -0,0 +1,74 @@
+import PropTypes from "prop-types";
+import React from "react";
+
+interface IServiceDeleteProps {
+  service: any;
+  onCancel?: () => void;
+}
+
+class ServiceDelete extends React.Component<IServiceDeleteProps, {}> {
+  static contextTypes = {
+    locale: PropTypes.string,
+    localize: PropTypes.func,
+  };
+
+  componentWillUnmount() {
+    this.handelCancel();
+  }
+
+  render() {
+    const { localize, locale } = this.context;
+
+    return (
+      <React.Fragment>
+        <div className="row">
+          <div className="col s12">
+            <span className="card-infos sub">
+              {localize("Services.realy_delete_service", locale)}
+            </span>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col s5 offset-s7">
+            <div className="row nobottom">
+              <div className="col s6">
+                <a className={"waves-effect waves-light btn modal-close"} onClick={this.handelCancel}>
+                  {localize("Common.cancel", locale)}
+                </a>
+              </div>
+              <div className="col s6">
+                <a className="waves-effect waves-light btn modal-close" onClick={this.handleDeleteService}>
+                  {localize("Common.delete", locale)}
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+
+  handelCancel = () => {
+    const { onCancel } = this.props;
+
+    if (onCancel) {
+      onCancel();
+    }
+  }
+
+  handleDeleteService = () => {
+    const { service } = this.props;
+    const { localize, locale } = this.context;
+
+    if (!service) {
+      return;
+    }
+
+    $(".toast-service_delete_ok").remove();
+    Materialize.toast(localize("Services.service_delete_ok", locale), 2000, "toast-service_delete_ok");
+
+    this.handelCancel();
+  }
+}
+
+export default ServiceDelete;
--- app/components/Services/ServiceSettings.tsx
+++ app/components/Services/ServiceSettings.tsx
@@ -2,15 +2,20 @@ import PropTypes from "prop-types";
 import React from "react";
 import { MEGAFON } from "../../service/megafon/constants";
 import BlockNotElements from "../BlockNotElements";
-import HeaderWorkspaceBlock from "../HeaderWorkspaceBlock";
+import Modal from "../Modal";
 import MegafonSettings from "./MegafonSettings";
+import ServiceDelete from "./ServiceDelete";
 import { IService } from "./types";
 
 interface IServiceSettingsProps {
   service: IService | undefined;
 }
 
-class ServiceSettings extends React.PureComponent<IServiceSettingsProps, {}> {
+interface IServiceSettingsState {
+  showModalDeleteService: boolean;
+}
+
+class ServiceSettings extends React.PureComponent<IServiceSettingsProps, IServiceSettingsState> {
   static contextTypes = {
     locale: PropTypes.string,
     localize: PropTypes.func,
@@ -18,16 +23,49 @@ class ServiceSettings extends React.PureComponent<IServiceSettingsProps, {}> {
 
   constructor(props: IServiceSettingsProps) {
     super(props);
+
+    this.state = ({
+      showModalDeleteService: false,
+    });
+  }
+
+  componentDidMount() {
+    $(".nav-small-btn, .file-setting-item").dropdown({
+      alignment: "left",
+      belowOrigin: false,
+      gutter: 0,
+      inDuration: 300,
+      outDuration: 225,
+    });
   }
 
   render() {
     const { localize, locale } = this.context;
     const { service } = this.props;
 
+    const DISABLED = service ? "" : "disabled";
+
     return (
       <div className="content-wrapper z-depth-1">
-        <HeaderWorkspaceBlock text={localize("Services.service_settings", locale)} />
+        <nav className="app-bar-content">
+          <ul className="app-bar-items">
+            <li className="app-bar-item">
+              <span>
+                {localize("Services.service_settings", locale)}
+              </span>
+            </li>
+            <li className="right">
+              <a className={"nav-small-btn waves-effect waves-light " + DISABLED} data-activates="dropdown-btn-for-service">
+                <i className="nav-small-icon material-icons cert-settings">more_vert</i>
+              </a>
+              <ul id="dropdown-btn-for-service" className="dropdown-content">
+                <li><a onClick={() => this.handleShowModalDeleteService()}>{localize("Services.delete", locale)}</a></li>
+              </ul>
+            </li>
+          </ul>
+        </nav>
         {this.getBody(service)}
+        {this.showModalDeleteService()}
       </div>
     );
   }
@@ -38,14 +76,44 @@ class ServiceSettings extends React.PureComponent<IServiceSettingsProps, {}> {
     if (service) {
       switch (service.type) {
         case MEGAFON:
-          return <MegafonSettings serviceId={service.id}/>;
+          return <MegafonSettings serviceId={service.id} />;
 
         default:
           return null;
       }
     } else {
-      return <BlockNotElements name="active" title={localize("Services.service_not_selected", locale)}/>;
+      return <BlockNotElements name="active" title={localize("Services.service_not_selected", locale)} />;
     }
+  }
+
+  showModalDeleteService = () => {
+    const { localize, locale } = this.context;
+    const { showModalDeleteService } = this.state;
+    const { service } = this.props;
+
+    if (!service || !showModalDeleteService) {
+      return;
+    }
+
+    return (
+      <Modal
+        isOpen={showModalDeleteService}
+        header={localize("Services.delete_service", locale)}
+        onClose={this.handleCloseModalDeleteService}>
+
+        <ServiceDelete
+          service={service}
+          onCancel={this.handleCloseModalDeleteService} />
+      </Modal>
+    );
+  }
+
+  handleShowModalDeleteService = () => {
+    this.setState({ showModalDeleteService: true });
+  }
+
+  handleCloseModalDeleteService = () => {
+    this.setState({ showModalDeleteService: false });
   }
 }
 
