21925d3020d1f0da0f30d92227f93edb021f8086
Aleksandr Gavrilov <alg@digt.ru>
2019-01-21 10:33:01 +0300

    Webview Vulnerability Fix
    https://electronjs.org/blog/webview-fix

--- app/main.development.js
+++ app/main.development.js
@@ -63,7 +63,7 @@ const installExtensions = async () => {
 
 
 app.on('window-all-closed', () => {
-    app.quit();
+  app.quit();
 });
 
 
@@ -194,5 +194,16 @@ app.on('ready', async () => {
   const menuBuilder = new MenuBuilder(mainWindow);
   menuBuilder.buildMenu();
 });
+
+app.on('web-contents-created', (event, win) => {
+  win.on('new-window', (event, newURL, frameName, disposition,
+    options, additionalFeatures) => {
+    if (!options.webPreferences) options.webPreferences = {};
+    options.webPreferences.nodeIntegration = false;
+    options.webPreferences.nodeIntegrationInWorker = false;
+    options.webPreferences.webviewTag = false;
+    delete options.webPreferences.preload;
+  })
+})
 
 
